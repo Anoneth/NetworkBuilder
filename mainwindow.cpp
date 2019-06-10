40fa3d245298f6b0bf9eb5d6ea89659b5f46173a
Anoneth <anoneth@ya.ru>
2019-06-10 19:43:13 +0400

    final fixes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     connect(ui->pushButtonCreate, SIGNAL(clicked()), this, SLOT(doJob()));
+    connect(ui->checkBoxParsim, SIGNAL(toggled(bool)), ui->spinBoxNodesCount, SLOT(setEnabled(bool)));
 }
 
 MainWindow::~MainWindow()
@@ -17,20 +18,28 @@ MainWindow::~MainWindow()
 void MainWindow::doJob()
 {
     int nodesCount = ui->spinBoxNodesCount->value();
-    int routersCount = ui->spinBoxRoutersCount->value();
-    int hostsPerSwitch = ui->spinBoxHostsPerRouter->value();
+    int size = ui->spinBoxSize->value();
     QString packageName = ui->lineEditPackageName->text();
     QString networkName = ui->lineEditNetworkName->text();
-    QString iniName = ui->lineEditIniName->text();
-    QString nedName = ui->lineEditNedName->text();
+    QString iniName = ui->lineEditIniFileName->text();
 
-    QString command = "python createNetwork.py " + QString::number(nodesCount) + " "
-            + QString::number(routersCount) + " "
-            + QString::number(hostsPerSwitch) + " "
-            + nedName + " "
-            + packageName + " "
-            + networkName + " "
-            + iniName;
-    QProcess::execute(command);
+    QStringList args;
+    args.push_back("networkCreator.py");
+    args.push_back("--size=" + QString::number(size));
+    args.push_back("--network-name=" + networkName);
+    args.push_back("--package-name=" + packageName);
+    args.push_back("--ini-filename=" + iniName);
+
+    if (ui->checkBoxParsim->isChecked())
+        args.push_back("--node-count=" + QString::number(nodesCount));
+
+    QProcess p;
+    p.setProgram("python");
+    p.setArguments(args);
+    p.start();
+    p.waitForReadyRead();
+    QMessageBox box;
+    box.setText(p.readAllStandardOutput());
+    box.exec();
 
 }
